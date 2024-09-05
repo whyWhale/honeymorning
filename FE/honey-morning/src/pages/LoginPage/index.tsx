5cefe38cf47f46c99ca7c1594104162eac72f935
kamusari <soccer_by@naver.com>
2024-09-05 14:03:21 +0900

    [front] S11P21A704-63 feat: 로그인 폼 입력란 생성

--- FE/honey-morning/src/pages/LoginPage/index.tsx
+++ FE/honey-morning/src/pages/LoginPage/index.tsx
@@ -1,4 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const SubmitButton = styled.button`
+  background-color: var(--mediumblue-color);
+  color: white;
+  padding: 1em 5em;
+  border: none;
+  rounded: 20%;
+  cursor: pointer;
+`;
+
+const MoveToSignUp = styled.button`
+  color: var(--mediumblue-color);
+  background-color: none;
+  cursor: pointer;
+`;
 
 const LoginProcess = () => {
   return (
@@ -7,16 +23,26 @@ const LoginProcess = () => {
       <form>
         <div>
           <label>Email</label>
+          <input
+            type="email"
+            placeholder="이메일을 입력해주세요."
+            required
+          />
         </div>
         <div>
           <label>Password</label>
+          <input
+            type="password"
+            placeholder="비밀번호를 입력해주세요."
+            required
+          />
         </div>
         <div>
-          <button>로그인</button>
+          <SubmitButton>로그인</SubmitButton>
         </div>
         <div>
           <div>아직 회원이 아니신가요?</div>
-          <button>회원가입 하기</button>
+          <MoveToSignUp>회원가입 하기</MoveToSignUp>
         </div>
       </form>
     </div>
