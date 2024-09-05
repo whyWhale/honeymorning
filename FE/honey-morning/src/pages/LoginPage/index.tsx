732d54458f39e0acfe15a172a5d263e1b3982e1b
kamusari <soccer_by@naver.com>
2024-09-05 11:37:45 +0900

    [front] S11P21A704-63 feat: 로그인 페이지 생성

---
+++ FE/honey-morning/src/pages/LoginPage/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const LoginProcess = () => {
+  return (
+    <div>
+      <div>Sign In</div>
+      <div>
+        <div>Email</div>
+      </div>
+      <div>
+        <div>Password</div>
+      </div>
+      <div>
+        <button>로그인</button>
+      </div>
+      <div>
+        <div>아직 회원이 아니신가요?</div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginProcess;
