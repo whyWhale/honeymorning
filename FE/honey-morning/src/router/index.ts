35bb3d985b3a8f2a6b71f72b54c022f4e42e0754
kamusari <soccer_by@naver.com>
2024-09-05 11:02:34 +0900

    [front] S11P21A704-63 feat: router 세팅

--- FE/honey-morning/src/router/index.ts
+++ FE/honey-morning/src/router/index.ts
@@ -1,11 +1,11 @@
 import React from 'react';
-import {createBrowserRouter, RouteObject} from 'react-router-dom';
+import {RouteObject} from 'react-router-dom';
 import AlarmSetting from '@/pages/AlarmSetting';
 
 const routerInfo: RouteObject[] = [
   {
     path: '/alarmsetting',
-    element: <AlarmSetting />, // 라우트에서 렌더링할 컴포넌트
+    element: React.createElement(AlarmSetting),
   },
 ];
 
