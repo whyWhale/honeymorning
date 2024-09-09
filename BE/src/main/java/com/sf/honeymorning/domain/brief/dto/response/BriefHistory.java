8aa8868086d5672967c213e9fe72be8e7d8b1e1c
kamusari <soccer_by@naver.com>
2024-09-09 17:11:01 +0900

    [back] S11P21A704-39 feat: 브리핑 목록 가져오기
    - 페이징 처리 완료

---
+++ BE/src/main/java/com/sf/honeymorning/domain/brief/dto/response/BriefHistory.java
@@ -0,0 +1,35 @@
+package com.sf.honeymorning.domain.brief.dto.response;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+@Schema(name = "브리핑 기록", description = "브리핑 목록 조회에서 필요한 응답 모델이에요 📦")
+@Getter
+public class BriefHistory {
+	@Schema(description = "브리프 ID", example = "1")
+	private final Long briefId;
+
+	@Schema(description = "생성 일자", example = "2023-09-10T12:30:00")
+	private final LocalDateTime createdAt;
+
+	@Schema(description = "카테고리 리스트", example = "['경제', '정치', '고냥이']")
+	private final List<String> categories;
+
+	@Schema(description = "브리핑 요약(음성 출력) ", example = "오늘의 날씨는 ... ... 입니다.")
+	private final String summary;
+
+	@Schema(description = "총 2문제 중 맞춘 정답수", example = "[0,1,2] 중 1개")
+	private final Long numberOfCorrectAnswer;
+
+	public BriefHistory(Long briefId, LocalDateTime createdAt, List<String> categories, String summary,
+		Long numberOfCorrectAnswer) {
+		this.briefId = briefId;
+		this.createdAt = createdAt;
+		this.categories = categories;
+		this.summary = summary;
+		this.numberOfCorrectAnswer = numberOfCorrectAnswer;
+	}
+}
---
+++ BE/src/main/java/com/sf/honeymorning/domain/brief/service/BriefService.java
@@ -0,0 +1,67 @@
+package com.sf.honeymorning.domain.brief.service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sf.honeymorning.domain.brief.dto.response.BriefHistory;
+import com.sf.honeymorning.domain.brief.entity.Brief;
+import com.sf.honeymorning.domain.brief.entity.BriefCategory;
+import com.sf.honeymorning.domain.brief.repository.BriefCategoryRepository;
+import com.sf.honeymorning.domain.brief.repository.BriefRepository;
+import com.sf.honeymorning.domain.brief.repository.QuizRepository;
+import com.sf.honeymorning.domain.common.Tag;
+import com.sf.honeymorning.domain.quiz.entity.Quiz;
+import com.sf.honeymorning.domain.user.entity.User;
+import com.sf.honeymorning.domain.user.repository.UserRepository;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class BriefService {
+	private final UserRepository userRepository;
+	private final BriefRepository briefRepository;
+	private final BriefCategoryRepository briefCategoryRepository;
+	private final QuizRepository quizRepository;
+
+	public List<BriefHistory> getBriefs(String authUsername, Pageable pageable) {
+		User user = userRepository.findByEmail(authUsername)
+			.orElseThrow(() -> new EntityNotFoundException("not exist user"));
+		Page<Brief> briefPage = briefRepository.findByUser(user,pageable);
+		List<Brief> briefs = briefPage.getContent();
+		List<BriefCategory> briefCategories = briefCategoryRepository.findByBrief(briefs);
+		List<Quiz> quizzes = quizRepository.findByBriefIn(briefs);
+
+		return toBriefHistoryDto(briefs, briefCategories, quizzes);
+	}
+
+	private List<BriefHistory> toBriefHistoryDto(List<Brief> briefs, List<BriefCategory> briefCategories, List<Quiz> quizzes) {
+		Map<Long, List<BriefCategory>> briefCategoryByBrief = briefCategories.stream()
+			.collect(Collectors.groupingBy(v -> v.getBrief().getId()));
+		Map<Long, List<Quiz>> quizzesByBrief = quizzes.stream()
+			.collect(Collectors.groupingBy(v -> v.getBrief().getId()));
+
+		return briefs.stream().map(brief -> {
+			return new BriefHistory(
+				brief.getId(),
+				brief.getCreatedAt(),
+				briefCategoryByBrief.get(brief.getId()).stream()
+					.map(BriefCategory::getTag)
+					.map(Tag::getWord)
+					.toList(),
+				brief.getSummary(),
+				quizzesByBrief.get(brief.getId()).stream()
+					.filter(quiz -> quiz.getAnswer() == quiz.getSelection())
+					.count()
+			);
+		}).toList();
+	}
+}
