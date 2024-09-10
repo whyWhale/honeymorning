b09ee18890d3e0c90936af0d4766a7f80e8340b9
kamusari <soccer_by@naver.com>
2024-09-10 13:34:45 +0900

    [front] S11P21A704-14 feat: 알람 시각에 따른 알람 페이지 이동, 예시 알람음 재생 기능 구현

--- FE/honey-morning/src/router/index.tsx
+++ FE/honey-morning/src/router/index.tsx
@@ -3,12 +3,17 @@ import {RouteObject} from 'react-router-dom';
 import SignInPage from '@/pages/SignInPage';
 import SignUpPage from '@/pages/SignUpPage';
 
+import AlarmPage from '@/pages/Alarm';
 import AlarmSetting from '@/pages/AlarmSetting';
 import MyPage from '@/pages/MyPage';
 import BriefingDetail from '@/pages/BriefingDetail';
 import SleepPage from '@/pages/Sleep';
 
 const routerInfo: RouteObject[] = [
+  {
+    path: '/alarm',
+    element: <AlarmPage />,
+  },
   {
     path: '/alarmsetting',
     element: <AlarmSetting />,
