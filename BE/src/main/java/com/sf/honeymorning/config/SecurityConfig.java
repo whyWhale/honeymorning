cb09d1058c612ec7dc1ef2a1fe5ab64a7061ae64
kamusari <soccer_by@naver.com>
2024-09-12 14:29:20 +0900

    [infra] S11P21A704-156 fix: sprign security 허가 경로 수정. swagger 동작을 위한 코드 수정

--- BE/src/main/java/com/sf/honeymorning/config/SecurityConfig.java
+++ BE/src/main/java/com/sf/honeymorning/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.sf.honeymorning.domain.auth.filter.LoginFilter;
 import com.sf.honeymorning.domain.auth.repository.RefreshTokenRepository;
 import com.sf.honeymorning.domain.auth.util.JWTUtil;
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.Collections;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -22,96 +23,100 @@ import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
-import java.util.Collections;
-
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-	@Value("${cors.allowedOrigins.frontend}")
-	private String allowedOrigins;
+    @Value("${cors.allowedOrigins.frontend}")
+    private String allowedOrigins;
 
-	private final AuthenticationConfiguration authenticationConfiguration;
-	private final JWTUtil jwtUtil;
-	private final RefreshTokenRepository refreshTokenRepository;
-
-
-	@Bean
-	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-		return configuration.getAuthenticationManager();
-	}
-
-	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder(){
-		return new BCryptPasswordEncoder();
-	}
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JWTUtil jwtUtil;
+    private final RefreshTokenRepository refreshTokenRepository;
 
 
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
+            throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 
-		http
-			.cors((cors) -> cors
-			.configurationSource(new CorsConfigurationSource() {
-
-				@Override
-				public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-
-					CorsConfiguration configuration = new CorsConfiguration();
-
-					// 허용할 포트
-					configuration.setAllowedOrigins(Collections.singletonList(allowedOrigins));
-					// 허용할 메서드
-					configuration.setAllowedMethods(Collections.singletonList("*"));
-					// 요청이 오는 곳의 credential을 true로 변경
-					configuration.setAllowCredentials(true);
-
-					configuration.setAllowedHeaders(Collections.singletonList("*"));
-					configuration.setMaxAge(3600L);
-
-					// 응답시 Authorization을 같이 보내준다.
-					configuration.setExposedHeaders(Collections.singletonList("Authorization"));
-
-					return configuration;
-				}
-
-			}));
-
-		// 경로별 인가
-		http
-				.authorizeHttpRequests((auth) -> auth
-					.requestMatchers("/api/auth/test").authenticated()
-					.requestMatchers("/api/auth/**", "api/users/check/email", "/swagger", "/swagger-ui.html", "/swagger-ui/**", "/api-docs", "/api-docs/**", "/v3/api-docs/**").permitAll()
-					.requestMatchers("/error", "/favicon.ico", "/**/*.png", "/**/*.gif", "/**/*.svg", "/**/*.jpg", "/**/*.html", "/**/*.css", "/**/*.js").permitAll()
-					.anyRequest().authenticated());
-
-		http
-				.csrf(csrf -> csrf.disable());
-
-		// jwt 권한 필터
-		http.addFilterBefore(new JWTFilter(jwtUtil),
-				UsernamePasswordAuthenticationFilter.class);
-
-		// 로그인 필터
-		http.addFilterAt(
-				new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil,
-						refreshTokenRepository),
-				UsernamePasswordAuthenticationFilter.class);
-
-		// 로그아웃 필터
-		http
-				.addFilterBefore(new CustomLogoutFilter(jwtUtil, refreshTokenRepository), LogoutFilter.class); // spring security가 제공하는 logoutfilter 바로 앞에서 실행ㄹ
-
-		//세션 설정
-		http.sessionManagement((session) -> session
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 
-		return http.build();
-	}
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+        http
+                .cors((cors) -> cors
+                        .configurationSource(new CorsConfigurationSource() {
 
+                            @Override
+                            public CorsConfiguration getCorsConfiguration(
+                                    HttpServletRequest request) {
+
+                                CorsConfiguration configuration = new CorsConfiguration();
+
+                                // 허용할 포트
+                                configuration.setAllowedOrigins(
+                                        Collections.singletonList(allowedOrigins));
+                                // 허용할 메서드
+                                configuration.setAllowedMethods(Collections.singletonList("*"));
+                                // 요청이 오는 곳의 credential을 true로 변경
+                                configuration.setAllowCredentials(true);
+
+                                configuration.setAllowedHeaders(Collections.singletonList("*"));
+                                configuration.setMaxAge(3600L);
+
+                                // 응답시 Authorization을 같이 보내준다.
+                                configuration.setExposedHeaders(
+                                        Collections.singletonList("Authorization"));
+
+                                return configuration;
+                            }
+
+                        }));
+
+        // 경로별 인가
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/api/auth/test").authenticated()
+                        .requestMatchers("/api/auth/**", "api/users/check/email", "/swagger",
+                                "/swagger-ui.html", "/swagger-ui/**", "/api-docs", "/api-docs/**",
+                                "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/error", "/favicon.ico", "/**/*.png", "/**/*.gif",
+                                "/**/*.svg", "/**/*.jpg", "/**/*.html", "/**/*.css", "/**/*.js")
+                        .permitAll()
+                        .anyRequest().permitAll()); // TODO : swagger 이후 고치기
+
+        http
+                .csrf(csrf -> csrf.disable());
+
+        // jwt 권한 필터
+        http.addFilterBefore(new JWTFilter(jwtUtil),
+                UsernamePasswordAuthenticationFilter.class);
+
+        // 로그인 필터
+        http.addFilterAt(
+                new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil,
+                        refreshTokenRepository),
+                UsernamePasswordAuthenticationFilter.class);
+
+        // 로그아웃 필터
+        http
+                .addFilterBefore(new CustomLogoutFilter(jwtUtil, refreshTokenRepository),
+                        LogoutFilter.class); // spring security가 제공하는 logoutfilter 바로 앞에서 실행ㄹ
+
+        //세션 설정
+        http.sessionManagement((session) -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
 
 
 }
