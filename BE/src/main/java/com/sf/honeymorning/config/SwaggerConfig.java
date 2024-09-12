c59773e023af459784d6f70fb83d4267e62692cd
kamusari <soccer_by@naver.com>
2024-09-12 14:45:43 +0900

    [infra] S11P21A704-156 fix: swagger 동작을 위한 코드 수정

--- BE/src/main/java/com/sf/honeymorning/config/SwaggerConfig.java
+++ BE/src/main/java/com/sf/honeymorning/config/SwaggerConfig.java
@@ -1,29 +1,33 @@
 package com.sf.honeymorning.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-
 @Configuration
 public class SwaggerConfig {
-	@Value("${spring.application.name}")
-	String applicationName;
 
-	@Bean
-	public OpenAPI springShopOpenAPI() {
-		return new OpenAPI()
-			.info(new Info().title(applicationName + " API 명세서")
-				.version("0.1")
-				.license(
-					new License()
-						.name(applicationName)
-						.url("https://www.honey-morining.com")
-				)
-			);
-	}
+    @Value("${spring.application.name}")
+    String applicationName;
+
+    @Value("${backend.server.url}")
+    String backendServerUrl;
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url(backendServerUrl)) // TODO : 배포를 위함
+                .info(new Info().title(applicationName + " API 명세서")
+                        .version("0.1")
+                        .license(
+                                new License()
+                                        .name(applicationName)
+                                        .url("https://www.honey-morining.com")
+                        )
+                );
+    }
 }
-
