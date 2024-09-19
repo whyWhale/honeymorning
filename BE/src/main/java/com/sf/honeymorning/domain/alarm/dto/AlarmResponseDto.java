170a03202323e5f87ea2c16053c5bc6d780dad80
kamusari <soccer_by@naver.com>
2024-09-19 18:03:41 +0900

    [back] S11P21A704-194 feat: swagger example 일부 작성

--- BE/src/main/java/com/sf/honeymorning/domain/alarm/dto/AlarmResponseDto.java
+++ BE/src/main/java/com/sf/honeymorning/domain/alarm/dto/AlarmResponseDto.java
@@ -1,5 +1,6 @@
 package com.sf.honeymorning.domain.alarm.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,9 +10,13 @@ import java.time.LocalTime;
 @Data
 @Builder
 @AllArgsConstructor
+@Schema(description = "알람 응답 DTO")
 public class AlarmResponseDto {
+    @Schema(description = "아이디")
     private Long id;
+    @Schema(description = "알람 시각", example = "07:00:00")
     private LocalTime alarmTime;
+    @Schema(description = "요일", example = "1111111", allowableValues = {"0000000", "0000001", "0010000", "0101010"})
     private Integer daysOfWeek;
     private Integer repeatFrequency;
     private Integer repeatInterval;
