22745e2c6674d05254138a69f5b61557f3bad129
kamusari <soccer_by@naver.com>
2024-09-23 11:20:06 +0900

    [back] S11P21A704-218 feat: 퀴즈 업데이트 및 퀴즈 조회 api 구현

--- BE/src/main/java/com/sf/honeymorning/domain/quiz/controller/QuizController.java
+++ BE/src/main/java/com/sf/honeymorning/domain/quiz/controller/QuizController.java
@@ -10,9 +10,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "퀴즈")
 @RequestMapping("/api/quizzes")
@@ -23,19 +21,34 @@ public class QuizController {
     private final QuizService quizService;
 
     @Operation(
-            summary = "퀴즈 전체 결과 확인"
+            summary = "퀴즈 확인"
     )
     @ApiResponses(value = {
             @ApiResponse(
                     responseCode = "200",
-                    description = "퀴즈 결과 가져오기 성공",
+                    description = "퀴즈 가져오기 성공",
                     content = @Content(schema = @Schema(type = "string", example = "success", implementation = QuizDto.class))
             )
     })
-    @GetMapping("/results")
-    public ResponseEntity<?> show() {
-        return quizService.getQuiz();
+    @GetMapping("/{briefId}")
+    public ResponseEntity<?> showAllResults(@PathVariable Long briefId) {
+        return quizService.getQuiz(briefId);
+    }
+
+    @Operation(
+            summary = "퀴즈 결과 업데이트"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "퀴즈 결과 업데이트 성공",
+                    content = @Content(schema = @Schema(type = "string", example = "success", implementation = QuizDto.class))
+            )
+    })
+    @PatchMapping
+    public ResponseEntity<?> updateQuizResults(@RequestBody QuizDto quizDto) {
+        return quizService.updateQuiz(quizDto);
     }
 
 }
-	
+
--- BE/src/main/java/com/sf/honeymorning/domain/quiz/repository/QuizRepository.java
+++ BE/src/main/java/com/sf/honeymorning/domain/quiz/repository/QuizRepository.java
@@ -1,12 +1,13 @@
 package com.sf.honeymorning.domain.quiz.repository;
 
+import com.sf.honeymorning.domain.brief.entity.Brief;
 import com.sf.honeymorning.domain.quiz.entity.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
-    @Query("SELECT q FROM Quiz q WHERE q.brief.user.id = :userId")
-    List<Quiz> findByUserId(Long userId);
+    List<Quiz> findByBriefIn(List<Brief> briefs);
+
+    List<Quiz> findByBrief(Brief brief);
 }
