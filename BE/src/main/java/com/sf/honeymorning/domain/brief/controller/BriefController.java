b7bbff887dddde2c58933a21169108885d459bf0
kamusari <soccer_by@naver.com>
2024-09-24 13:35:37 +0900

    [back] S11P21A704-273 feat: 백엔드 브리핑과 FastAPI 브리핑 통신 테스트 완료 [설정변경]

--- BE/src/main/java/com/sf/honeymorning/domain/brief/controller/BriefController.java
+++ BE/src/main/java/com/sf/honeymorning/domain/brief/controller/BriefController.java
@@ -1,17 +1,8 @@
 package com.sf.honeymorning.domain.brief.controller;
 
-import org.springframework.data.domain.PageRequest;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.sf.honeymorning.domain.brief.dto.response.BriefHistoryResponseDto;
 import com.sf.honeymorning.domain.brief.dto.response.BriefDetailResponseDto;
+import com.sf.honeymorning.domain.brief.dto.response.BriefHistoryResponseDto;
 import com.sf.honeymorning.domain.brief.service.BriefService;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -21,6 +12,13 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @Valid
 @RequiredArgsConstructor
@@ -28,40 +26,48 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/briefs")
 @RestController
 public class BriefController {
-	private final BriefService briefService;
 
-	@Operation(
-		summary = "브리핑 상세 조회"
-	)
-	@ApiResponses(value = {
-		@ApiResponse(
-			responseCode = "200",
-			description = "상세 조회 성공",
-			content = @Content(schema = @Schema(implementation = BriefDetailResponseDto.class))
-		)
-	})
-	@GetMapping("/{brief_id}")
-	public ResponseEntity<BriefDetailResponseDto> read(
-		@Parameter(description = "조회할 브리핑의 ID", example = "12345")
-		@PathVariable(name = "brief_id") Long briefId) {
-		BriefDetailResponseDto data = briefService.getBrief(briefId);
+    private final BriefService briefService;
 
-		return ResponseEntity.ok(data);
-	}
+    @Operation(
+            summary = "브리핑 상세 조회"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "상세 조회 성공",
+                    content = @Content(schema = @Schema(implementation = BriefDetailResponseDto.class))
+            )
+    })
+    @GetMapping("/{brief_id}")
+    public ResponseEntity<BriefDetailResponseDto> read(
+            @Parameter(description = "조회할 브리핑의 ID", example = "12345")
+            @PathVariable(name = "brief_id") Long briefId) {
+        BriefDetailResponseDto data = briefService.getBrief(briefId);
 
-	@Operation(
-		summary = "브리핑 전체 조회"
-	)
-	@ApiResponses(value = {
-		@ApiResponse(
-			responseCode = "200",
-			description = "전체 조회 성공",
-			content = @Content(schema = @Schema(implementation = BriefHistoryResponseDto.class))
-		)
-	})
-	@GetMapping("/all")
-	public ResponseEntity<BriefHistoryResponseDto> readAll(@RequestParam(value = "page") Integer page) {
-		BriefHistoryResponseDto briefs = briefService.getBriefs(page);
-		return ResponseEntity.ok(briefs);
-	}
+        return ResponseEntity.ok(data);
+    }
+
+    @Operation(
+            summary = "브리핑 전체 조회"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "전체 조회 성공",
+                    content = @Content(schema = @Schema(implementation = BriefHistoryResponseDto.class))
+            )
+    })
+    @GetMapping("/all")
+    public ResponseEntity<BriefHistoryResponseDto> readAll(
+            @RequestParam(value = "page") Integer page) {
+        BriefHistoryResponseDto briefs = briefService.getBriefs(page);
+        return ResponseEntity.ok(briefs);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test(@RequestBody String text) {
+        String brief = briefService.storeBriefFromAi(text);
+        return ResponseEntity.ok(brief);
+    }
 }
