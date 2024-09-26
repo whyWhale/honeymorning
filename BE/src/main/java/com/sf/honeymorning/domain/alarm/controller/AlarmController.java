1a3c448c31b9db890c67b201beed56e21cee33f5
kamusari <soccer_by@naver.com>
2024-09-27 08:57:19 +0900

    [infra] S11P21A704-273 fix: 백엔드 application.properties 오류 수정

--- BE/src/main/java/com/sf/honeymorning/domain/alarm/controller/AlarmController.java
+++ BE/src/main/java/com/sf/honeymorning/domain/alarm/controller/AlarmController.java
@@ -16,7 +16,12 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @Tag(name = "알람")
@@ -24,8 +29,10 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @AllArgsConstructor
 public class AlarmController {
+
     private final AlarmService alarmService;
 
+    
     @GetMapping("/test")
     public ResponseEntity<?> test(HttpServletRequest req, HttpServletResponse res) {
         return ResponseEntity.ok("success");
