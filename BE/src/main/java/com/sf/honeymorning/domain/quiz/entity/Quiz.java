a8a21645ddf915529213ccc70fe5dd6c11a62db5
kamusari <soccer_by@naver.com>
2024-09-30 09:54:32 +0900

    [back] S11P21A704-327 feat: Alarm, Brief 엔티티에 filepath 경로 필드 추가

--- BE/src/main/java/com/sf/honeymorning/domain/quiz/entity/Quiz.java
+++ BE/src/main/java/com/sf/honeymorning/domain/quiz/entity/Quiz.java
@@ -3,8 +3,20 @@ package com.sf.honeymorning.domain.quiz.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.sf.honeymorning.domain.brief.entity.Brief;
 import com.sf.honeymorning.domain.common.entity.BaseEntity;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
@@ -40,5 +52,8 @@ public class Quiz extends BaseEntity {
     private String option4;
 
     private Integer selection;
+
+    @Column(length = 1000, nullable = true)
+    private String summaryFilePath;
 
 }
