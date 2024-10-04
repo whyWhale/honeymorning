00e0e2fe633a0376b225b53f2dd14487598e6872
kamusari <soccer_by@naver.com>
2024-10-04 10:46:05 +0900

    [back] S11P21A704-326 fix: AllArgsContructro -> Required로 변경

--- BE/src/main/java/com/sf/honeymorning/domain/quiz/service/QuizService.java
+++ BE/src/main/java/com/sf/honeymorning/domain/quiz/service/QuizService.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -24,7 +25,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Transactional
 @Slf4j
 public class QuizService {
