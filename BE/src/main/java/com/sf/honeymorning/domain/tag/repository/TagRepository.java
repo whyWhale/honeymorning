e95f0a2691c803178e40e2a4974c6578debdb42c
kamusari <soccer_by@naver.com>
2024-10-07 15:48:25 +0900

    [back] S11P21A704-362 feat: 알람 카테고리 수정 api 추가

--- BE/src/main/java/com/sf/honeymorning/domain/tag/repository/TagRepository.java
+++ BE/src/main/java/com/sf/honeymorning/domain/tag/repository/TagRepository.java
@@ -3,6 +3,8 @@ package com.sf.honeymorning.domain.tag.repository;
 import com.sf.honeymorning.domain.tag.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TagRepository extends JpaRepository<Tag, Long> {
-    Tag findByWord(String word);
+    Optional<Tag> findByWord(String word);
 }
